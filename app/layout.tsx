62ff64d04b173ea8eeb500f5f1bbf285537c817f
Hasan Goli <mrhasangoli@gmail.com>
2023-10-28 21:37:21 +0330

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+import { PropsWithChildren } from 'react';
+
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
